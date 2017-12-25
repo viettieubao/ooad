07d2f76b47537d7e88d5a7fb9967af3a1ff793c4
viettieubao <anhhungtieubao@gmail.com>
2017-12-25 22:40:15 +0700

    Đã xong Connecttion.

---
+++ QuanLiVatLieuXayDung/QuanLiVatLieuXayDung/Model/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text;
+
+namespace QuanLiVatLieuXayDung.Model
+{
+    class Connection
+    {
+        public static SqlConnection sqlConnection;
+        public static SqlCommand sqlCommand;
+        public static SqlDataAdapter sqlDataAdapter;
+        public static DataTable dataTable;
+
+        public static string conn = "Data Source=DESKTOP-J4CPLKG;Initial Catalog=OOAD;Integrated Security=True";
+        public static DataTable getData(String sql)
+        {
+            sqlConnection = new SqlConnection(conn);
+            sqlConnection.Open();
+            sqlDataAdapter = new SqlDataAdapter(sql,sqlConnection);
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            return dataTable;
+        }
+
+        public static int ExcuteNonQuery(String sql)
+        {
+            int result = 0;
+            try
+            {
+                sqlConnection = new SqlConnection(conn);
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(sql, sqlConnection);
+                result = sqlCommand.ExecuteNonQuery();
+                sqlConnection.Close();
+
+            }
+            catch
+            {
+                
+            }
+            return result;
+        }
+    }
+}
---
+++ QuanLiVatLieuXayDung/QuanLiVatLieuXayDung/Model/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QuanLiVatLieuXayDung.Model
+{
+    class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Bophan { get; set; }
+        public bool IsActive { get; set; }
+        public string Tennhanvien { get; set; }
+        public string Diachi { get; set; }
+        public DateTime Ngaysing { get; set; }
+        public string Sodienthoai { get; set; }
+
+        public bool GetUserByUserName(string Username, string Pass)
+        {
+            string command = @"select * from User where username='" + Username + @"' and password='" + Pass + @"'";
+            DataTable dt = Connection.getData(command);
+            if (dt.Rows.Count != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public DataTable GetAllUser()
+        {
+            string command = @"select username, tennhanvien, ngaysinh, sodienthoai, diachi from User";
+            return Connection.getData(command);
+        }
+    }
+}
